ba678c14d8d7827fa6a66e2be967d2823e01c01e
Aleksandr Surzhko <aleksandr.surzhko@r1.team>
2024-05-14 02:02:05 +0500

    check game

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import WebApp from '@twa-dev/sdk';
 
 function App() {
-    const [count, setCount] = useState(0);
     const onClick = () => {
         WebApp.showAlert(`Hello ${WebApp.initDataUnsafe.user.first_name} ${WebApp.initDataUnsafe.user.last_name}`);
     };
@@ -22,15 +20,10 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
+                <a href={'/game'} type={'button'}>
+                    Play Game
+                </a>
             </div>
-            <div className="card">
-                <button onClick={onClick}>Show Alert</button>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
         </>
     );
 }
