e343639f46d62493826a06e43fbc2223fff4cfab
Aleksandr Surzhko <aleksandr.surzhko@r1.team>
2024-05-14 02:06:22 +0500

    fix link

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <a href={'/game'} type={'button'}>
+                <a href={'game.html'} type={'button'}>
                     Play Game
                 </a>
             </div>
