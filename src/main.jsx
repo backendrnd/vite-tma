3043ca51ae6c04a52aea252a8244f7e6d8808d84
Aleksandr Surzhko <aleksandr.surzhko@r1.team>
2024-06-06 04:43:29 +0500

    fix scrolling (2)

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import WebApp from '@twa-dev/sdk';
 import { AppStoreProvider } from './stores/AppProvider';
 
 WebApp.expand();
-window.addEventListener('touchmove', (e) => e.preventDefault(), { passive: false });
-window.scrollTo(0, 100);
+// window.addEventListener('touchmove', (e) => e.preventDefault(), { passive: false });
+// window.scrollTo(0, 100);
 WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
