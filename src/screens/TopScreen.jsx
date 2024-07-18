30573ad1a01d6d587d14f8938acbd8ae68228b63
Aleksandr Surzhko <aleksandr.surzhko@r1.team>
2024-07-18 15:05:09 +0500

    TopScreen css update

--- src/screens/TopScreen.jsx
+++ src/screens/TopScreen.jsx
@@ -1,10 +1,14 @@
 import './Top.css';
 import { useEffect, useState } from 'react';
 import api from '../api/Api.js';
-import { COIN_TOKEN } from '../constants/main.js';
+import { useAppStore } from '../stores/AppProvider.jsx';
+import { getLevelByExperience } from '../helpers/ExperienceHelper.js';
+
+const WebApp = window.Telegram.WebApp;
 
 function TopScreen() {
     const [users, setUsers] = useState(/** @type {User[]} */ []);
+    const appStore = useAppStore();
 
     useEffect(() => {
         async function fetchData() {
@@ -15,26 +19,45 @@ function TopScreen() {
     }, []);
 
     return (
-        <div className="top">
-            <table className="table is-fullwidth">
-                <thead>
-                    <tr>
-                        <th>Rank</th>
-                        <th>Name</th>
-                        <th>Experience</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user, index) => (
-                        <tr key={user.id}>
-                            <td>{index + 1}</td>
-                            <td className={index === 0 ? 'gold' : ''}>{user.username}</td>
-                            <td>{Number(user.experience).toLocaleString()}</td>
+        <>
+            <div className="hero-body p-0 is-flex-direction-column is-fullheight-withrank">
+                <div className="container is-fullwidth is-clipped">
+                    <table className="table is-fullwidth is-striped">
+                        <thead>
+                            <tr>
+                                <th>Rank</th>
+                                <th>Name</th>
+                                <th>Level</th>
+                                <th>Exp</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user, index) => (
+                                <tr key={user.id}>
+                                    <td>{index + 1}</td>
+                                    <td className={index === 0 ? 'gold' : ''}>{user.username}</td>
+                                    <td>{getLevelByExperience(Number(user.experience))}</td>
+                                    <td>{Number(user.experience).toLocaleString()}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="hero-foot">
+                <span className="has-text-weight-bold pl-2">Character Rank</span>
+                <table className="table is-fullwidth is-striped">
+                    <thead>
+                        <tr className="is-main">
+                            <th>?</th>
+                            <th>{WebApp?.initDataUnsafe?.user?.first_name}</th>
+                            <th>{getLevelByExperience(appStore.experience)}</th>
+                            <th>{appStore.experience.toLocaleString()}</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                </table>
+            </div>
+        </>
     );
 }
 
